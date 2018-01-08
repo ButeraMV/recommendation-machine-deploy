954773e186985d394839674426466086394da723
Boots2291 <buteramv@gmail.com>
2018-01-08 00:12:18 -0700

    Remove function that was not being used yet

--- src/components/ratings-page/RatingsPage.js
+++ src/components/ratings-page/RatingsPage.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import MovieList from './MovieList'
 
+// the Link submit button is going to post to flask to add the user's known preferences
+
 class RatingsPage extends Component {
   render() {
     return (
       <div className='ratings-page'>
         <h2>Please rate the following movies:</h2>
         <MovieList />
-        <Link to='/recommendations'><button onClick={this.postRatings.bind(this)}>Submit</button></Link>
+        <Link to='/recommendations'><button>Submit</button></Link>
       </div>
     );
   }
